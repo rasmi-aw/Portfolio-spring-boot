8e7e36d72230895a98c7f0833abbba30d29487ba
rasmi-aw <abdelwadoudrasmi@gmail.com>
2023-10-03 18:56:29 +0200

    Adding data models

--- src/main/java/com/beastwall/portfoliospringboot/model/Data.java
+++ src/main/java/com/beastwall/portfoliospringboot/model/Data.java
@@ -2,16 +2,13 @@ package com.beastwall.portfoliospringboot.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.experimental.Accessors;
 
 @AllArgsConstructor
-@NoArgsConstructor
 @Getter
 @Setter
 @Accessors(chain = true)
 public class Data {
-
 
 }
