960f5872d99c91f8e065592dff5f1de68e40be4d
rasmi-aw <abdelwadoudrasmi@gmail.com>
2023-10-03 16:29:01 +0200

    initial commit

---
+++ src/main/java/com/beastwall/portfoliospringboot/PortfolioSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.beastwall.portfoliospringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortfolioSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PortfolioSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/beastwall/portfoliospringboot/PortfolioSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beastwall.portfoliospringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
