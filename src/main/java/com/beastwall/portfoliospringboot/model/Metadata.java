7f1dc3cf4e0e92d4a0233bb41e2a6a4efd6748c5
rasmi-aw <abdelwadoudrasmi@gmail.com>
2023-10-03 18:55:31 +0200

    Adding data models

---
+++ src/main/java/com/beastwall/portfoliospringboot/model/Metadata.java
@@ -0,0 +1,21 @@
+package com.beastwall.portfoliospringboot.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.Accessors;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Accessors(chain = true)
+public class Metadata {
+    private String title;
+    private String description;
+    @JsonProperty("og:img")
+    private String image;
+    private String favicon;
+}
