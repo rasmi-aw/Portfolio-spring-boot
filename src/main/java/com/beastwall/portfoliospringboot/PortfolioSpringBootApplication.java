82644f4d0baae513e6ceea56029da6f23e229098
rasmi-aw <abdelwadoudrasmi@gmail.com>
2023-10-03 16:41:14 +0200

    initial commit

--- src/main/java/com/beastwall/portfoliospringboot/PortfolioSpringBootApplication.java
+++ src/main/java/com/beastwall/portfoliospringboot/PortfolioSpringBootApplication.java
@@ -2,12 +2,22 @@ package com.beastwall.portfoliospringboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
+
+@RequestMapping("/")
+@Controller
 public class PortfolioSpringBootApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(PortfolioSpringBootApplication.class, args);
     }
 
+
+    @RequestMapping("/")
+    public String root() {
+        return "index";
+    }
 }
