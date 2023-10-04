c8060f7ce9213f53fe153599429bfcb5a3900184
rasmi-aw <abdelwadoudrasmi@gmail.com>
2023-10-04 13:00:13 +0200

    update classes

--- src/main/java/com/beastwall/portfoliospringboot/model/Activity.java
+++ src/main/java/com/beastwall/portfoliospringboot/model/Activity.java
@@ -2,10 +2,12 @@ package com.beastwall.portfoliospringboot.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.experimental.Accessors;
 
 @AllArgsConstructor
+@NoArgsConstructor
 @Getter
 @Setter
 @Accessors(chain = true)
