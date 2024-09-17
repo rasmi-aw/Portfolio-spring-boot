7d054e04792a736388d08dff0f6ba56a8b1ff3e1
rasmi <abdelwadoudrasmi@gmail.com>
2024-09-17 16:43:45 +0200

    refactoring

--- src/main/java/com/beastwall/portfoliospringboot/config/BeastEngineConfig.java
+++ src/main/java/com/beastwall/portfoliospringboot/config/BeastEngineConfig.java
@@ -1,6 +1,6 @@
 package com.beastwall.portfoliospringboot.config;
 
-import com.beastwall.beaststack.engine.BeastHtmlEngine;
+import com.beastwall.beastengine.BeastHtmlEngine;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.AllArgsConstructor;
