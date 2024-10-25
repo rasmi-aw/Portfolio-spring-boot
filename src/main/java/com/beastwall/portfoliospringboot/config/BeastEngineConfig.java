9dc21ae8a3f3b241f5571ce17949abb8a08d9a7f
rasmi <abdelwadoudrasmi@gmail.com>
2024-10-25 17:52:10 +0200

    Refactoring: beast version for spring boot

--- src/main/java/com/beastwall/portfoliospringboot/config/BeastEngineConfig.java
+++ src/main/java/com/beastwall/portfoliospringboot/config/BeastEngineConfig.java
@@ -6,6 +6,8 @@ import com.beastwall.beastengine.Context;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.AllArgsConstructor;
+import org.openjdk.nashorn.api.scripting.NashornScriptEngineFactory;
+import org.openjdk.nashorn.internal.runtime.ScriptEnvironment;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -15,6 +17,8 @@ import org.springframework.web.servlet.View;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
 import java.util.Locale;
 import java.util.Map;
 
